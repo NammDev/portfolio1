1cbfab9c2446a20ac8cfdfa6c137756dda0a2392
Nam <namdeveloper.ca@gmail.com>
2023-11-05 11:21:42 +0700

    about

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from '@/components/about'
 import Intro from '@/components/intro'
 import SectionDivider from '@/components/section-divider'
 
@@ -6,8 +7,8 @@ export default function Home() {
     <main className='flex flex-col items-center px-4'>
       <Intro />
       <SectionDivider />
-      {/* <About />
-      <Projects />
+      <About />
+      {/* <Projects />
       <Skills />
       <Experience />
       <Contact /> */}
